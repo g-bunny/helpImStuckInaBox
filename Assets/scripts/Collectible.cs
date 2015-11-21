e91f7d9ab0d5b211f8c2ab2c40e0fde929ab029f
g-bunny <astarrynite@gmail.com>
2015-11-21 11:57:29 -0500

    trying to make chimney dynamic via collectible.cs. not yet working

--- Assets/scripts/Collectible.cs
+++ Assets/scripts/Collectible.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class Collectible : MonoBehaviour {
 	public static int collectCount;
-
+	public Transform player;
+	private Transform chimney = GameObject.Find("Chimney").GetComponent<Transform>();
+	Vector3 chimneyPos = chimney.localScale;
 	void Start () {
+		chimney.localScale.x = 0f;
 	}
 	
 	void Update () {
 		Debug.Log(collectCount);
+		if (collectCount >= 4){
+			chimney.localScale.x = 1f;
+		}
 	}
 	void OnTriggerEnter(Collider target)
 	{
